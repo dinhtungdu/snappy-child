f966f171efb9454f1b60ca9c9817d57b7fde77e2
dinhtungdu <dinhtungdu@gmail.com>
2015-08-07 13:26:00 +0700

    Pull master back to skeleton

--- functions.php
+++ functions.php
@@ -9,84 +9,23 @@ if( ! function_exists( 'snappy_child_scripts' ) ) :
 function snappy_child_scripts() {
 	wp_enqueue_style( 'snappy-style-child', get_stylesheet_uri(), false, null );
   wp_enqueue_script( 'snappy-child-js', get_stylesheet_directory_uri() . '/inc/custom.js', array(), '20141012', true );
-	wp_enqueue_script( 'snappy-child-masonry-js', get_template_directory_uri() . '/framework/lib/masonry/dist/masonry.pkgd.min.js', array(), '20150807', true );
 }
 endif;
 add_action( 'wp_enqueue_scripts', 'snappy_child_scripts', 20 );
 
 function snappy_child_section($sections){
   $sections[] = array(
-    'title' => __('ToiPhuNu.com', 'snappy-child'),
-    'desc' => __('<p class="description">This is a section created by adding a filter to the sections array. Can be used by child themes to add/remove sections from the options.</p>', 'snappy-child'),
+    'title' => __('Child theme extra options', 'snappy-child'),
+    'desc' => __('<p class="description">Specific options for child theme.</p>', 'snappy-child'),
     'icon' => 'paper-clip',
     'icon_class' => 'icon-large',
     // Leave this as a blank section, no options just some intro text set above.
     'fields' => array(
-      array(
-          'id'        => 'tpn_slider_ids',
-          'type'      => 'multi_text',
-          'title'     => __('Post Slider', 'snappy-opts'),
-          'subtitle'  => __('Enter Posts id for display in slider', 'snappy-opts'),
-      ),
-      array(
-          'id'        => 'tpn_banner_1_img',
-          'type'      => 'media',
-          'title'     => __('Banner 1 Image', 'snappy-opts'),
-          'url'       => true,
-          'readonly'  => false,
-      ),
-      array(
-          'id'        => 'tpn_banner_1_title',
-          'type'      => 'text',
-          'title'     => __('Banner 1 Title', 'snappy-opts'),
-      ),
-      array(
-          'id'        => 'tpn_banner_1_url',
-          'type'      => 'text',
-          'title'     => __('Banner 1 URL', 'snappy-opts'),
-          'default'   => '#'
-      ),
-      array(
-          'id'        => 'tpn_banner_2_img',
-          'type'      => 'media',
-          'title'     => __('Banner 2 Image', 'snappy-opts'),
-          'url'       => true,
-          'readonly'  => false,
-      ),
-      array(
-          'id'        => 'tpn_banner_2_title',
-          'type'      => 'text',
-          'title'     => __('Banner 2 Title', 'snappy-opts'),
-      ),
-      array(
-          'id'        => 'tpn_banner_2_url',
-          'type'      => 'text',
-          'title'     => __('Banner 2 URL', 'snappy-opts'),
-          'default'   => '#'
-      ),
-      array(
-        'title'     => __('Chọn chuyên mục tin cho trang chủ', 'snappy-opts'),
-        'type'      => 'select',
-        'id'        => 'tpn_home_cat',
-        'data'      => 'categories',
-        'multi'     => true,
-        'sortable'  => true,
-        'select2'   => array( 'maximumSelectionSize' => '3' ),
-      ),
-      array(
-          'id'        => 'tpn_featured_ids',
-          'type'      => 'multi_text',
-          'title'     => __('Bài viết Đọc nhiều Sidebar', 'snappy-opts'),
-          'subtitle'  => __('Enter Posts id for display in slider', 'snappy-opts'),
-      ),
     ),
   );
 
   return $sections;
 }
-add_filter('redux/options/snappy/sections', 'snappy_child_section');
+// add_filter('redux/options/snappy/sections', 'snappy_child_section');
 
-set_post_thumbnail_size( 270, 220, true );
-
-include_once( dirname( __FILE__ ) . '/inc/widget-featured.php' );
-include_once( dirname( __FILE__ ) . '/inc/widget-category-posts.php' );
+//set_post_thumbnail_size( 270, 220, true );
